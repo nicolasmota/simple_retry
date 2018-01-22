b1c220b1777b532d4290dc9f8e9c31218b73f3dc
nickmota <nicolas_mota@me.com>
2018-01-22 10:27:01 -0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'simple_retry',
+    packages = ['simple_retry'],
+    version = '0.1',
+    description = 'A simple retry decorator',
+    author = 'Nicolas Mota',
+    author_email = 'nicolas_mota@me.com',
+    url = 'https://github.com/nicolasmota/simple_retry',
+    download_url = 'https://github.com/nicolasmota/simple_retry/archive/0.1.tar.gz',
+    keywords = ['retry', 'decorator', 'http', 'requests'],
+    classifiers = [],
+)
