d3b013ce74c1876ee21733ef5d5d51818b26bbc0
Henrique Braga <henrique.inftec@hotmail.com>
2018-07-07 14:09:04 -0300

    Create coroutine retry

--- simple_retry/decorators.py
+++ simple_retry/decorators.py
@@ -2,6 +2,8 @@ import time
 
 from functools import wraps
 
+import asyncio
+
 
 def retry(Except, retries=5, delay=0, logger=None, level='info', multiple=1):
 
@@ -28,5 +30,35 @@ def retry(Except, retries=5, delay=0, logger=None, level='info', multiple=1):
                     mdelay *= multiple
                     tries += 1
             return function(*args, **kwargs)
+        return f_retry
+    return deco_retry
+
+
+def coroutine_retry(Except, retries=5, delay=0, logger=None, level='info', multiple=1):
+
+    def deco_retry(function):
+
+        @asyncio.coroutine
+        @wraps(function)
+        def f_retry(*args, **kwargs):
+            tries = 1
+            mdelay = delay
+            while tries < retries:
+                try:
+                    return (yield from (function(*args, **kwargs)))
+                except Except as e:
+                    msg = '{e}, Retrying {tries} of {retries}'.format(
+                        e=e,
+                        tries=tries,
+                        retries=retries
+                    )
+                    if mdelay:
+                        msg = ' '.join([msg, 'in {} seconds...'.format(mdelay)])
+                    if logger:
+                        getattr(logger, level)(msg)
+                    time.sleep(mdelay)
+                    mdelay *= multiple
+                    tries += 1
+            return (yield from function(*args, **kwargs))
         return f_retry
     return deco_retry
--- tests/test_retry.py
+++ tests/test_retry.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
 import logging
+
+import asyncio
 import mock
 import pytest
 
-from ..simple_retry.decorators import retry
+
+from ..simple_retry.decorators import retry, coroutine_retry
 
 logger = logging.getLogger(__file__)
 
@@ -22,4 +25,19 @@ class TestSimpleRetryDecorator:
         with mock.patch('os.urandom') as mock_fun:
             with pytest.raises(Exception):
                 funct_retried()
+        assert mock_fun.call_count == 5
+
+
+@coroutine_retry(Exception, retries=5, delay=1, logger=logger)
+def coroutine_func_retried():
+    yield from asyncio.sleep(1)
+    raise Exception('test')
+
+
+class TestCoroutineSimpleRetryDecorator:
+
+    def test_coroutine_retries_n_times(self):
+        with mock.patch('asyncio.sleep') as mock_fun:
+            with pytest.raises(Exception):
+                 yield from (coroutine_func_retried())
         assert mock_fun.call_count == 5
