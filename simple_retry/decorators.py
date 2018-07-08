9c3f242823c2e60fcd55c99b85671cdeb5301c7c
Henrique Braga <henrique.inftec@hotmail.com>
2018-07-08 15:13:55 -0300

    Create async retry decorator

--- simple_retry/decorators.py
+++ simple_retry/decorators.py
@@ -62,3 +62,31 @@ def coroutine_retry(Except, retries=5, delay=0, logger=None, level='info', multi
             return (yield from function(*args, **kwargs))
         return f_retry
     return deco_retry
+
+
+def async_retry(Except, retries=5, delay=0, logger=None, level='info', multiple=1):
+    def deco_retry(function):
+
+        @wraps(function)
+        async def f_retry(*args, **kwargs):
+            tries = 1
+            mdelay = delay
+            while tries < retries:
+                try:
+                    return await (function(*args, **kwargs))
+                except Except as e:
+                    msg = '{e}, Retrying {tries} of {retries}'.format(
+                        e=e,
+                        tries=tries,
+                        retries=retries
+                    )
+                    if mdelay:
+                        msg = ' '.join([msg, 'in {} seconds...'.format(mdelay)])
+                    if logger:
+                        getattr(logger, level)(msg)
+                    time.sleep(mdelay)
+                    mdelay *= multiple
+                    tries += 1
+            return await function(*args, **kwargs)
+        return f_retry
+    return deco_retry
--- tests/test_retry.py
+++ tests/test_retry.py
@@ -7,7 +7,7 @@ import mock
 import pytest
 
 
-from ..simple_retry.decorators import retry, coroutine_retry
+from ..simple_retry.decorators import retry, coroutine_retry, async_retry
 
 logger = logging.getLogger(__file__)
 
@@ -40,4 +40,24 @@ class TestCoroutineSimpleRetryDecorator:
         with mock.patch('asyncio.sleep') as mock_fun:
             with pytest.raises(Exception):
                  yield from (coroutine_func_retried())
+        assert mock_fun.call_count == 5
+
+
+@async_retry(Exception, retries=5, delay=1, logger=logger)
+async def async_func_retried():
+    await asyncio.sleep(1)
+    raise Exception('test')
+
+
+class TestAsyncSimpleRetryDecorator:
+
+    @pytest.mark.asyncio
+    async def test_async_retries_n_times(self):
+        with mock.patch.object(asyncio, 'sleep') as mock_fun:
+            mock_coro = mock.Mock(name='result', return_value=True)
+            mock_fun.side_effect = asyncio.coroutine(mock_coro)
+
+            with pytest.raises(Exception):
+                await async_func_retried()
+
         assert mock_fun.call_count == 5
